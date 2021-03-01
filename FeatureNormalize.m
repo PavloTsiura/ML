13545d43880cab33c86049b2237685852d675d9c
PavloTsiura <43480580+PavloTsiura@users.noreply.github.com>
2021-03-02 01:06:49 +0200

    Create FeatureNormalize.m

---
+++ FeatureNormalize.m
@@ -0,0 +1,13 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. 
+
+
+mu = mean(X, 1);
+sigma = std(X);
+X_norm = (X - mu) ./ sigma;
+
+
+end
