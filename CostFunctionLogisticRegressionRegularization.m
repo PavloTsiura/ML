f529cef3a0bb91ed9881125291312af965a115ec
PavloTsiura <43480580+PavloTsiura@users.noreply.github.com>
2021-03-07 14:40:06 +0200

    Update CostFunctionLogisticRegressionRegularization.m

--- CostFunctionLogisticRegressionRegularization.m
+++ CostFunctionLogisticRegressionRegularization.m
@@ -7,8 +7,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 h = sigmoid(X * theta);
-J = ( sum( -y .* log(h) - (1 - y) .* log(1 - h) ) ./ m ) + lambda / ...
-    (2 * m) .* sum(theta(2:end) .^2);
+J = ( sum( -y .* log(h) - (1 - y) .* log(1 - h) ) ./ m ) + lambda / (2 * m) .* sum(theta(2:end) .^2);
 
 grad = sum( (h - y) .* X) / m;
 grad(:, 2:end) = grad(2:end) + lambda / m .* theta(2:end)';
